2e93d49b78d2149bb7481cd03abfb356230633e7
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-02 13:28:26 +0530

    feature: Create components for Header & Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import HomePage from '@/pages/HomePage.tsx';
+import Header from '@/components/Header.tsx';
+
 function App() {
-    return <h1>RecipeDiary</h1>;
+    return (
+        <div>
+            <Header />
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
