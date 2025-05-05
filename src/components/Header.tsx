98b44d520e8ebc21af0566e3b28da041f4f6082d
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-05 11:42:36 +0530

    feature: Style the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <header className="w-full max-w-[100rem] m-auto h-16 fixed left-0 right-0 flex glassmorphic border-b border-slate-900">
+        <header className="z-50 w-full max-w-[100rem] m-auto h-16 fixed left-0 right-0 flex glassmorphic">
             <div className="w-fit flex justify-center items-center px-2 cursor-pointer">
                 <img className="h-12" src="/logos/recipediary_white.png" />
             </div>
