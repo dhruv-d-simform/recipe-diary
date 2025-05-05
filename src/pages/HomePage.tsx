45d0cf7359db9c6449fea0a03cffd1bcc328ee6f
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-05 11:44:42 +0530

    feature: Render the Recipe Cards in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,14 @@
+import { recipes } from '@/dummyData';
+import RecipeCard from '@/components/RecipeCard';
+
 function HomePage() {
-    return <div>Home Page</div>;
+    return (
+        <div className="grid grid-cols-auto-fit p-8 gap-8">
+            {recipes.map((recipe) => (
+                <RecipeCard recipe={recipe} key={recipe.id} />
+            ))}
+        </div>
+    );
 }
 
 export default HomePage;
