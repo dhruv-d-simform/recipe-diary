0637d0ea4814419858fdcfc8c1e56f7ad41fec94
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-05 15:05:52 +0530

    feature: Change the logo in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,17 @@ function Header() {
     return (
         <header className="z-50 w-full max-w-[100rem] m-auto h-16 fixed left-0 right-0 flex glassmorphic">
             <div
-                className="w-fit flex justify-center items-center px-2 cursor-pointer"
+                className="w-fit flex justify-center items-center gap-1 px-2 cursor-pointer"
                 tabIndex={0}
             >
                 <img
-                    className="h-12"
-                    src="/logos/recipediary_white.png"
+                    className="h-11"
+                    src="/logos/recipediary.png"
                     alt="Recipe Diary Logo"
                 />
+                <p role="heading" className="text-[#ffd086] text-2xl">
+                    RecipeDiary
+                </p>
             </div>
         </header>
     );
