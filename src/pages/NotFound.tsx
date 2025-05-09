99bf8be5237d8046aac8e2ff2bfd08191ddeeeac
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-09 14:42:58 +0530

    feature: Create NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+    return (
+        <div className="h-[85dvh] flex flex-col justify-center items-center">
+            <p className="text-9xl">404</p>
+            <p className="text-2xl">Not Found</p>
+        </div>
+    );
+}
