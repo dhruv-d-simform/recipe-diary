541d18cb75c8f976d407295f55dcb3dc68c2288d
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-09 14:47:13 +0530

    feature: Setup the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { HomePage } from '@/pages/HomePage.tsx';
 import { Header } from '@/components/Header.tsx';
+import { Outlet } from 'react-router';
 
 export function App() {
     return (
         <div>
             <Header />
             <div className="w-full max-w-[100rem] m-auto pt-16">
-                <HomePage />
+                <Outlet />
             </div>
         </div>
     );
